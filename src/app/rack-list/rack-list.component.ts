4073d4bc55366d0fd41a75ea3dbcc473e5892896
djinn506 <eltarantula@gmail.com>
2022-11-13 02:55:23 -0300

    Add Styles to racklist

--- src/app/rack-list/rack-list.component.ts
+++ src/app/rack-list/rack-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Rack } from './rack';
+
 
 @Component({
   selector: 'rack-list',
@@ -7,12 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RackListComponent implements OnInit {
 
-  rack = {
+  racks : Rack[] = [{
     brand : "NVIDIA",
     model: "3060",
     type : "gpu",
-    image: "assets/NVDIA3060.jpg"
-  }
+    image: "assets/NVDIA3060.jpg",
+    fail: false,
+  },
+  {
+    brand : "AMD",
+    model: "5700 XT",
+    type : "gpu",
+    image: "assets/AMD5700XT.jpeg",
+    fail: true,
+  },
+  {
+    brand : "Kingston",
+    model: "Fury 8Gb DDR4",
+    type : "ram",
+    image: "assets/KingstonFury8Gb.jpeg",
+    fail: false,
+  },
+]
 
   constructor() { }
 
---
+++ src/app/rack-list/rack.ts
@@ -0,0 +1,7 @@
+export interface Rack {
+    brand : string,
+    model: string,
+    type : string,
+    image : string,
+    fail : boolean,
+    }
