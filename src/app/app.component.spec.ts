b667392447c8e31922846e1e3be289fd26c12f71
djinn506 <djinn506@gmail.com>
2023-10-26 19:39:04 -0300

    Revert break test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'stacksapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('stacksfailapp');
+    expect(app.title).toEqual('stacksapp');
   });
 
   it('should render title', () => {
