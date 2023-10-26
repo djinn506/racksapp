e73c56448ccb7ed20c22f8577fd40f3ca4402894
djinn506 <djinn506@gmail.com>
2023-10-26 20:24:23 -0300

    Update karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     /* browsers: ['Firefox'], */
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+    browsers: ['ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
