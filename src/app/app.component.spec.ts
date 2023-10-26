5b08dabc246cf8a6d73b34e1afc68ac98ef28c8f
djinn506 <djinn506@gmail.com>
2023-10-26 19:51:56 -0300

    Add test homeComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -26,10 +27,18 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('stacksapp');
   });
 
-  it('should render title', () => {
+  /* it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('stacksapp app is running!');
+  }); */
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Manage and Organize your Servers Here');
   });
+
 });
