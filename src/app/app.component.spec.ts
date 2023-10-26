35fd97dac873b6df53a9f5099e20c39274cddeca
djinn506 <djinn506@gmail.com>
2023-10-26 19:33:29 -0300

    Break Test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'stacksapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('stacksapp');
+    expect(app.title).toEqual('stacksfailapp');
   });
 
   it('should render title', () => {
