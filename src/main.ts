aed0eba6094ec2547413f9ed9e31aad801548149
djinn506 <djinn506@gmail.com>
2023-11-16 18:56:47 -0300

    Attempt Fix Cors

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { NestFactory } from '@nestjs/core';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-
-  async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.enableCors();
-    await app.listen(process.env['PORT']  || 3000);
-  }
-  bootstrap();
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
