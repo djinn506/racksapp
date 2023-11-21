980a600699cffcdf90f5e96dac6afa265741966b
djinn506 <djinn506@gmail.com>
2023-11-20 22:59:11 -0300

    Fix url in server data service

--- src/app/server-data.service.ts
+++ src/app/server-data.service.ts
@@ -1,26 +1,26 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Server } from './server-list/server';
 
-const URL = "https://racks-api2.onrender.com/server";
+const URL = environment.apiUrl + 'server';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ServerDataService {
-
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-  }
-  
-  constructor(private http:HttpClient) { }
+      'Content-Type': 'application/json',
+    }),
+  };
 
-  getAll():Observable<Server[]> {
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Server[]> {
     return this.http.get<Server[]>(URL);
   }
-  createServer(stack :any): Observable<Server> {
+  createServer(stack: any): Observable<Server> {
     return this.http.post<Server>(URL, stack, this.httpOptions);
   }
 }
